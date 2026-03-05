597f464434d2363a822664ea5a2482b15f1abb0c
otaviorodriguess <otaviorodriguesbezerra71@gmail.com>
2026-03-05 00:39:51 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/otavio/fluxo_financeiro/FluxoFinanceiroApplication.java
@@ -0,0 +1,11 @@
+package com.otavio.fluxo_financeiro ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoFinanceiroApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FluxoFinanceiroApplication.class, args);
+	}
+}
